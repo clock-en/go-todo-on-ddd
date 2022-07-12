b18baea9c4b2133b4d6ea83820b3283b9f7c3da9
clock-en <k.kuroda.biz@gmail.com>
2022-07-12 01:54:09 +0000

    fix: todosテーブルをtasksテーブルに修正

---
+++ src/app/domain/vo/task_content.go
@@ -0,0 +1,25 @@
+package vo
+
+import (
+	"fmt"
+)
+
+// TaskContent represents value obejct user name
+type TaskContent struct {
+	value string
+}
+
+func NewTaskContent(value string) (*TaskContent, error) {
+	if isInvalidLengthTaskContent(value) {
+		return nil, fmt.Errorf("todo task must be no longer than 140 characters")
+	}
+	return &TaskContent{value: value}, nil
+}
+
+func (t TaskContent) Value() string {
+	return t.value
+}
+
+func isInvalidLengthTaskContent(value string) bool {
+	return 140 < len(value)
+}
